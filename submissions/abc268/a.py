5b21c822e92d93348ea7874bde7bab4856250241
goto <104724026+gomatofu@users.noreply.github.com>
2022-10-09 20:39:55 +0900

    Delete submissions directory

--- submissions/abc268/a.py
+++
@@ -1,3 +0,0 @@
-l=list(map(int,input().split()))
-result = list(set(l))
-print(len(result))
--- submissions/abc272/a.py
+++
@@ -1,6 +0,0 @@
-n=int(input())
-l=list(map(int,input().split()))
-sum=0
-for i in range(n):
-  sum+=l[i]
-print(sum)
