155f4c61c397c08ed1ca6aeaedfc43e496d53b4d
goto <gogo8bit@yahoo.co.jp>
2022-11-03 23:23:03 +0900

    add submission: 2022/11/03 23:23:03

---
+++ submissions/abc135/c.py
@@ -0,0 +1,12 @@
+n=int(input())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+
+ans=0
+for i in range(n):
+  ans+=min(a[i],b[i])
+  b[i]-=min(a[i],b[i])
+  ans+=min(a[i+1],b[i])
+  a[i+1]-=min(a[i+1],b[i])
+    
+print(ans)
