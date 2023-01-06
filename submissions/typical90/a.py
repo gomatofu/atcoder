d6d4062238d2ee6a05076dab39792fe38621b484
goto <gogo8bit@yahoo.co.jp>
2023-01-06 16:07:30 +0900

    delete

--- submissions/typical90/a.py
+++
@@ -1,9 +0,0 @@
-n=int(input())
-
-lis=set()
-
-for i in range(n):
-  a=input()
-  if a not in lis:
-    print(i+1)
-    lis.add(a)
--- submissions/typical90/g.py
+++
@@ -1,8 +0,0 @@
-import math
-h,w=map(int,input().split())
-if h==1 or w==1:
-  print(h*w)
-else:
-  h=math.ceil(h/2)
-  w=math.ceil(w/2)
-  print(w*h)
