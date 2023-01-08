e67716876a53953cc8a1c266f0dd8c9587b60691
goto <gogo8bit@yahoo.co.jp>
2023-01-09 00:55:59 +0900

    add submission: 2023/01/09 00:55:59

--- submissions/typical90/b.py
+++ submissions/typical90/b.py
@@ -1,14 +1,22 @@
-import itertools
-n = int(input())
- 
-for S in itertools.product(['(',')'], repeat=n):
+from itertools import product
+import sys
+def II(): return int(sys.stdin.readline().rstrip())
+def LI(): return list(map(int,sys.stdin.readline().rstrip().split()))
+def SS(): return sys.stdin.readline().rstrip()
+def LS(): return list(sys.stdin.readline().rstrip().split())
+N = II()
+
+for pro in product(('(', ')'), repeat=N):
     cnt = 0
-    for s in S:
-        if s =='(':
+
+    for p in pro:
+        if p == '(':
             cnt+=1
         else:
             cnt-=1
+        
         if cnt < 0:
             break
-    if cnt ==0:
-        print(*S,sep='')
+    
+    if cnt == 0:
+        print(*pro,sep='')
