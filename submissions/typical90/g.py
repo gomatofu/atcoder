f3a84ff3f012f8154b111c74b01a3b577384b52c
goto <gogo8bit@yahoo.co.jp>
2023-01-14 21:31:07 +0900

    add submission: 2023/01/14 21:31:07

--- submissions/typical90/g.py
+++ submissions/typical90/g.py
@@ -1,12 +1,35 @@
-from bisect import bisect_left
-n=int(input())
-a=list(map(int,input().split()))
-a.sort()
-q=int(input())
-for i in range(q):
-    b=int(input())
-    l=bisect_left(a,b)
-    if n==l:
-      print(abs(a[l-1]-b))
-    else:
-      print(min(abs(a[l-1]-b),abs(a[l]-b)))
+import sys
+def II(): return int(sys.stdin.readline().rstrip())
+def LI(): return list(map(int,sys.stdin.readline().rstrip().split()))
+def SS(): return sys.stdin.readline().rstrip()
+def LS(): return list(sys.stdin.readline().rstrip().split())
+N = II()
+A = LI()
+Q = II()
+A.sort()
+
+def is_ok(mid,B):
+     if A[mid] <= B:
+         return True
+     else:
+         return False
+ 
+def binary_search(left, right,B): 
+     while right - left > 1:
+         mid = (left + right) // 2
+         if is_ok(mid,B):
+             left = mid
+         else:
+             right = mid
+     return left , right
+ 
+for i in range(Q):
+    B = int(input())
+    left,right = binary_search(0,N,B)
+    if left < 0:
+        left = 0
+
+    if right >= N:
+        right = N-1
+    ans = min(abs(A[left]-B),abs(A[right]-B))
+    print(ans)
