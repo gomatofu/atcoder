3bdc3ffb5492be06f3051a3244000c512605fdca
goto <gogo8bit@yahoo.co.jp>
2023-01-23 23:08:12 +0900

    add submission: 2023/01/23 23:08:12

--- submissions/abc132/a.py
+++ submissions/abc132/a.py
@@ -1,6 +1,13 @@
-s=list(input())
-a=len(set(s))
-if a==2:
+s=input()
+l=[]
+for i in range(4):
+  l.append(s[i])
+l.sort()
+if l[1] == l[2]:
+  print('No')
+elif l[0]==l[1] and l[2]==l[3]:
   print('Yes')
 else:
   print('No')
+    
+    
